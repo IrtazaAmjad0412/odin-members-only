a8990e91817e030ea78f3e45c0cf176539b2cb65
IrtazaAmjad0412 <irtaza0412@gmail.com>
2026-01-13 14:47:37 -0500

    feat: configured dotenv, EJS view engine, and default PORT in app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const PORT = 3000;
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
