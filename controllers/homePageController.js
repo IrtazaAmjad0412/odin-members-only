26de59fbb471920d186dd8f164a6e5beca7ee187
IrtazaAmjad0412 <irtaza0412@gmail.com>
2026-01-14 11:35:07 -0500

    feat: update header and homepage structure for improved layout and consistency

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -3,7 +3,11 @@ import { getAllPosts } from "../db/queries.js";
 export const getAllPostsAndRenderHomePage = async (req, res) => {
   try {
     const posts = await getAllPosts();
-    res.render("homePage", { title: "Odin Clubhouse", posts: posts });
+    res.render("homePage", {
+      appTitle: "Odin Clubhouse",
+      pageTitle: "All Posts",
+      posts: posts,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,6 @@
 export const renderSignUpForm = (req, res) => {
   try {
-    res.render("signUpForm", { title: "Sign-In Form" });
+    res.render("signUpForm", { appTitle: "Odin Clubhouse", pageTitle: "Sign Up Form" });
   } catch (err) {
     console.error(err);
     res.status(500).send("Server Error");
