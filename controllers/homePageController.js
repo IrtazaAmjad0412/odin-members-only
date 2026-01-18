ff182c2157ec3b456b98748a07236be5b57c1aa7
IrtazaAmjad0412 <irtaza0412@gmail.com>
2026-01-17 21:19:55 -0500

    feat: restrict author and date visibility to non-members on home page

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -3,9 +3,17 @@ import { getAllPosts } from "../db/queries.js";
 export const getAllPostsAndRenderHomePage = async (req, res) => {
   try {
     const posts = await getAllPosts();
+    const isMember = req.user && req.user.membership_status === "member";
+    const postsToRender = isMember
+      ? posts
+      : posts.map((post) => ({
+          ...post,
+          author: null,
+          created_at: null,
+        }));
     res.render("homePage", {
       appTitle: "Odin Clubhouse",
-      posts: posts,
+      posts: postsToRender,
     });
   } catch (err) {
     console.error(err);
